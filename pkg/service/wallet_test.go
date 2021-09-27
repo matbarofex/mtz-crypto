b16657d6ca5d93f86316aa190267acc734ae85b7
Santiago Fernandez <sfernandez@matriz.com.ar>
2021-09-27 12:16:05 -0300

    Uso de decimales con precisión arbitraria

--- pkg/service/wallet_test.go
+++ pkg/service/wallet_test.go
@@ -7,14 +7,15 @@ import (
 	"github.com/matbarofex/mtz-crypto/mocks"
 	"github.com/matbarofex/mtz-crypto/pkg/model"
 	"github.com/matbarofex/mtz-crypto/pkg/store/memory"
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetWalletValue(t *testing.T) {
 	items := []model.WalletItem{
-		{Symbol: "SYM1", Quantity: 0.1},
-		{Symbol: "SYM2", Quantity: 0.1},
-		{Symbol: "SYM3", Quantity: 0.1},
+		{Symbol: "SYM1", Quantity: decimal.RequireFromString("0.1")},
+		{Symbol: "SYM2", Quantity: decimal.RequireFromString("0.1")},
+		{Symbol: "SYM3", Quantity: decimal.RequireFromString("0.1")},
 	}
 	wallet := model.Wallet{
 		ID:    "wallet1",
@@ -27,19 +28,19 @@ func TestGetWalletValue(t *testing.T) {
 	ts1, _ := time.Parse(time.RFC3339, "2021-09-23T12:34:56Z")
 	mdStore.SetOrUpdateMD(model.MarketData{
 		Symbol:            "SYM1",
-		LastPrice:         1.0,
+		LastPrice:         decimal.RequireFromString("1.0"),
 		LastPriceDateTime: ts1,
 	})
 	ts2, _ := time.Parse(time.RFC3339, "2021-09-23T13:34:56Z")
 	mdStore.SetOrUpdateMD(model.MarketData{
 		Symbol:            "SYM2",
-		LastPrice:         1.0,
+		LastPrice:         decimal.RequireFromString("1.0"),
 		LastPriceDateTime: ts2,
 	})
 	ts3, _ := time.Parse(time.RFC3339, "2021-09-23T14:34:56Z")
 	mdStore.SetOrUpdateMD(model.MarketData{
 		Symbol:            "SYM3",
-		LastPrice:         1.0,
+		LastPrice:         decimal.RequireFromString("1.0"),
 		LastPriceDateTime: ts3,
 	})
 
@@ -52,6 +53,6 @@ func TestGetWalletValue(t *testing.T) {
 	resp, err := walletService.GetWalletValue(req)
 
 	assert.NoError(t, err)
-	assert.Equal(t, 0.3, *resp.Value)
+	assert.Equal(t, "0.3", resp.Value.Decimal.String())
 	assert.Equal(t, ts3, *resp.DateTime)
 }
